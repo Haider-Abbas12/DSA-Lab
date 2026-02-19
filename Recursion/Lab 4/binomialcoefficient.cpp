c24da6ed200e4fe4a476fa08fdb49069b982f91b
haider-abbas12 <haiderabbas5147@gmail.com>
2026-02-19 09:40:49 +0500

    Third task

---
+++ Recursion/Lab 4/binomialcoefficient.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int binomialCoefficient(int n, int k) {
+    if (k == 0 || k == n)
+        return 1;
+    return binomialCoefficient(n - 1, k - 1) + binomialCoefficient(n - 1, k);
+}
