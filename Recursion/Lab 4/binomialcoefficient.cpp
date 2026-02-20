9c5205c4f552f2807da5961e556b6a6f87a2ed18
haider-abbas12 <haiderabbas5147@gmail.com>
2026-02-20 16:54:23 +0500

    Last Lab Task

--- Recursion/Lab 4/binomialcoefficient.cpp
+++ Recursion/Lab 4/binomialcoefficient.cpp
@@ -6,3 +6,19 @@ int binomialCoefficient(int n, int k) {
         return 1;
     return binomialCoefficient(n - 1, k - 1) + binomialCoefficient(n - 1, k);
 }
+
+int main() {
+    int n, k;
+    cout << "Enter n: ";
+    cin >> n;
+    cout << "Enter k: ";
+    cin >> k;
+
+    if (k < 0 || k > n) {
+        cout << "Invalid input: k must be between 0 and n." << endl;
+    } else {
+        cout << "C(" << n << ", " << k << ") = " << binomialCoefficient(n, k) << endl;
+    }
+
+    return 0;
+}
